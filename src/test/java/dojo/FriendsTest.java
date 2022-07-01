a848cc07a6bc535859e7e721e4096585ba4aed65
Guillermo Segura <guillermo.segura@axity.com>
2022-07-01 16:33:19 -0500

    Ajustes

--- src/test/java/dojo/FriendsTest.java
+++ src/test/java/dojo/FriendsTest.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -25,7 +26,7 @@ import org.junit.jupiter.api.Test;
 class FriendsTest
 {
 
-  private Set<Node> friends = new HashSet<>();
+  private List<Node> friends = new ArrayList<>();
 
   @BeforeEach
   protected void init()
@@ -37,6 +38,39 @@ class FriendsTest
     this.addFriend( people, "Matthew", "David" );
     this.addFriend( people, "Matthew", "Maggie" );
     this.addFriend( people, "Matthew", "Tristan" );
+
+    for( Entry<String, List<String>> entry : people.entrySet() )
+    {
+      Node test = new Node();
+      test.setName( entry.getKey() );
+
+      if( friends.contains( test ) )
+      {
+        test = friends.get( friends.indexOf( test ) );
+      }
+      else
+      {
+        friends.add( test );
+      }
+
+      for( String name : entry.getValue() )
+      {
+        Node friend = new Node();
+        friend.setName( name );
+
+        if( friends.contains( friend ) )
+        {
+          friend = friends.get( friends.indexOf( friend ) );
+        }
+        else
+        {
+          friends.add( friend );
+        }
+
+        test.getNodes().add( friend );
+      }
+
+    }
   }
 
   private void addFriend( Map<String, List<String>> people, String person, String friend )
@@ -51,7 +85,7 @@ class FriendsTest
       list = new ArrayList<String>();
       people.put( person, list );
     }
-    list.add( friend )
+    list.add( friend );
   }
 
   @Test
