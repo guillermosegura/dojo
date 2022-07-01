5f7ca97597c43a8861aadce14f392afc56477160
Guillermo Segura <guillermo.segura@axity.com>
2022-07-01 15:57:09 -0500

    Método par agregar personas

--- src/test/java/dojo/FriendsTest.java
+++ src/test/java/dojo/FriendsTest.java
@@ -7,9 +7,13 @@
 // </summary>
 package dojo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -17,16 +21,37 @@ import org.junit.jupiter.api.Test;
 
 /**
  * @author guillermo.segura@axity.com
- *
  */
 class FriendsTest
 {
-  
+
   private Set<Node> friends = new HashSet<>();
-  
+
   @BeforeEach
-  protected void init() {
-        
+  protected void init()
+  {
+    Map<String, List<String>> people = new HashMap<>();
+    this.addFriend( people, "Matthew", "Luke" );
+    this.addFriend( people, "Matthew", "Brent" );
+    this.addFriend( people, "Matthew", "Michael" );
+    this.addFriend( people, "Matthew", "David" );
+    this.addFriend( people, "Matthew", "Maggie" );
+    this.addFriend( people, "Matthew", "Tristan" );
+  }
+
+  private void addFriend( Map<String, List<String>> people, String person, String friend )
+  {
+    List<String> list;
+    if( people.containsKey( person ) )
+    {
+      list = people.get( person );
+    }
+    else
+    {
+      list = new ArrayList<String>();
+      people.put( person, list );
+    }
+    list.add( friend )
   }
 
   @Test
